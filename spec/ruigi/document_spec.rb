34d8d17a32c0b23fb4ef75d122bc371a9fdf2ada
naoki-k <k12naoki@gmail.com>
2016-10-23 08:02:50 +0900

    Remove unused variable

--- spec/ruigi/document_spec.rb
+++ spec/ruigi/document_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Ruigi::Document do
   context 'When made a Document Object' do
     let(:str) { 'I have a pen but I want to another one' }
-    let(:document) { Ruigi::Document.new('sample', str.split) }
+    let(:document) { Ruigi::Document.new(str.split) }
     let!(:model) { Ruigi::Model.new([document]) }
 
     it 'has correct size for feature vector' do
--- spec/ruigi/word_spec.rb
+++ spec/ruigi/word_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Ruigi::Word do
   context 'When made a model' do
     let(:str) { 'I have a pen but I want to another one' }
-    let(:document) { Ruigi::Document.new('sample', str.split) }
+    let(:document) { Ruigi::Document.new(str.split) }
 
     it 'is counted correctly' do
       expect(document.words['I'].count).to eq(2)
