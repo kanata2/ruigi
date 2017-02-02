7a8370a1e13359e2c69d2ae9c13a08155068465d
Naoki Kanatani <k12naoki@gmail.com>
2017-02-02 17:24:18 +0900

    fix grammer

--- spec/ruigi/document_spec.rb
+++ spec/ruigi/document_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Ruigi::Document do
   context 'When made a Document Object' do
-    let(:str) { 'I have a pen but I want to another one' }
+    let(:str) { 'I have a pretty pen but I want another one' }
     let(:document) { Ruigi::Document.new(str.split) }
     let!(:model) { Ruigi::Model.new([document]) }
 
